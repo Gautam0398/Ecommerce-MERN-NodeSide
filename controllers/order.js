a5faf2f74b3797c00b21e4aab62056be8dabc741
GMweb1998 <51059904+GMweb1998@users.noreply.github.com>
2020-07-04 15:34:40 +0530

    changes

--- controllers/order.js
+++ controllers/order.js
@@ -1,46 +1,50 @@
-const { Order, CartItem } = require('../models/order');
-const { errorHandler } = require('../helpers/dbErrorHandler');
+require("dotenv").config();
+const { Order, CartItem } = require("../models/order");
+const { errorHandler } = require("../helpers/dbErrorHandler");
 // sendgrid for email npm i @sendgrid/mail
-const sgMail = require('@sendgrid/mail');
-sgMail.setApiKey('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');
+const sgMail = require("@sendgrid/mail");
+const { config } = require("dotenv/types");
+sgMail.setApiKey(`${config.env.API_KEY}`);
 
 exports.orderById = (req, res, next, id) => {
-    Order.findById(id)
-        .populate('products.product', 'name price')
-        .exec((err, order) => {
-            if (err || !order) {
-                return res.status(400).json({
-                    error: errorHandler(err)
-                });
-            }
-            req.order = order;
-            next();
+  Order.findById(id)
+    .populate("products.product", "name price")
+    .exec((err, order) => {
+      if (err || !order) {
+        return res.status(400).json({
+          error: errorHandler(err),
         });
+      }
+      req.order = order;
+      next();
+    });
 };
 
 exports.create = (req, res) => {
-    console.log('CREATE ORDER: ', req.body);
-    req.body.order.user = req.profile;
-    const order = new Order(req.body.order);
-    order.save((error, data) => {
-        if (error) {
-            return res.status(400).json({
-                error: errorHandler(error)
-            });
-        }
-        // send email alert to admin
-        // order.address
-        // order.products.length
-        // order.amount
-        const emailData = {
-            to: 'gautam.mehta@techchefz.com', // admin
-            from: 'noreply@ecommerce.com',
-            subject: `A new order is received`,
-            html: `
+  console.log("CREATE ORDER: ", req.body);
+  req.body.order.user = req.profile;
+  const order = new Order(req.body.order);
+  order.save((error, data) => {
+    if (error) {
+      return res.status(400).json({
+        error: errorHandler(error),
+      });
+    }
+    // send email alert to admin
+    // order.address
+    // order.products.length
+    // order.amount
+    const emailData = {
+      to: "gautam.mehta@techchefz.com", // admin
+      from: "noreply@ecommerce.com",
+      subject: `A new order is received`,
+      html: `
             <h1>Hey Admin, Somebody just made a purchase in your ecommerce store</h1>
             <h2>Customer name: ${order.user.name}</h2>
             <h2>Customer address: ${order.address}</h2>
-            <h2>User's purchase history: ${order.user.history.length} purchase</h2>
+            <h2>User's purchase history: ${
+              order.user.history.length
+            } purchase</h2>
             <h2>User's email: ${order.user.email}</h2>
             <h2>Total products: ${order.products.length}</h2>
             <h2>Transaction ID: ${order.transaction_id}</h2>
@@ -48,29 +52,30 @@ exports.create = (req, res) => {
             <h2>Product details:</h2>
             <hr />
             ${order.products
-                .map(p => {
-                    return `<div>
+              .map((p) => {
+                return `<div>
                         <h3>Product Name: ${p.name}</h3>
                         <h3>Product Price: ${p.price}</h3>
                         <h3>Product Quantity: ${p.count}</h3>
                 </div>`;
-                })
-                .join('--------------------')}
+              })
+              .join("--------------------")}
             <h2>Total order cost: ${order.amount}<h2>
             <p>Login to your dashboard</a> to see the order in detail.</p>
-        `
-        };
-        sgMail
-            .send(emailData)
-            .then(sent => console.log('SENT >>>', sent))
-            .catch(err => console.log('ERR >>>', err));
- 
-        // email to buyer
-        const emailData2 = {
-            to: order.user.email,
-            from: 'noreply@ecommerce.com',
-            subject: `You order is in process`,
-            html: `
+        `,
+    };
+
+    sgMail
+      .send(emailData)
+      .then((sent) => console.log("SENT >>>", sent))
+      .catch((err) => console.log("ERR >>>", err));
+
+    // email to buyer
+    const emailData2 = {
+      to: order.user.email,
+      from: "noreply@ecommerce.com",
+      subject: `You order is in process`,
+      html: `
             <h1>Hey ${req.profile.name}, Thank you for shopping with us.</h1>
             <h2>Total products: ${order.products.length}</h2>
             <h2>Transaction ID: ${order.transaction_id}</h2>
@@ -78,51 +83,56 @@ exports.create = (req, res) => {
             <h2>Product details:</h2>
             <hr />
             ${order.products
-                .map(p => {
-                    return `<div>
+              .map((p) => {
+                return `<div>
                         <h3>Product Name: ${p.name}</h3>
                         <h3>Product Price: ${p.price}</h3>
                         <h3>Product Quantity: ${p.count}</h3>
                 </div>`;
-                })
-                .join('--------------------')}
+              })
+              .join("--------------------")}
             <h2>Total order cost: ${order.amount}<h2>
             <p>Thank your for shopping with us.</p>
-        `
-        };
-        sgMail
-            .send(emailData2)
-            .then(sent => console.log('SENT 2 >>>', sent))
-            .catch(err => console.log('ERR 2 >>>', err));
-        res.json(data);
-    });
+        `,
+    };
+
+    // sgMail
+    //   .send(emailData2)
+    //   .then((sent) => console.log("SENT 2 >>>", sent))
+    //   .catch((err) => console.log("ERR 2 >>>", err));
+    res.json(data);
+  });
 };
 
 exports.listOrders = (req, res) => {
-    Order.find()
-        .populate('user', '_id name address')
-        .sort('-created')
-        .exec((err, orders) => {
-            if (err) {
-                return res.status(400).json({
-                    error: errorHandler(error)
-                });
-            }
-            res.json(orders);
+  Order.find()
+    .populate("user", "_id name address")
+    .sort("-created")
+    .exec((err, orders) => {
+      if (err) {
+        return res.status(400).json({
+          error: errorHandler(error),
         });
+      }
+      res.json(orders);
+    });
 };
 
 exports.getStatusValues = (req, res) => {
-    res.json(Order.schema.path('status').enumValues);
+  res.json(Order.schema.path("status").enumValues);
 };
 
 exports.updateOrderStatus = (req, res) => {
-    Order.update({ _id: req.body.orderId }, { $set: { status: req.body.status } }, (err, order) => {
-        if (err) {
-            return res.status(400).json({
-                error: errorHandler(err)
-            });
-        }
-        res.json(order);
-    });
+  Order.update(
+    { _id: req.body.orderId },
+    { $set: { status: req.body.status } },
+    (err, order) => {
+      if (err) {
+        return res.status(400).json({
+          error: errorHandler(err),
+        });
+      }
+      res.json(order);
+    }
+  );
 };
